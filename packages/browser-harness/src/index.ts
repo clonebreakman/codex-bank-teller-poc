cb7c08c775a21e26d73b8fbadf5b7e7042d75beb
clonebreakman <115345656+clonebreakman@users.noreply.github.com>
2026-08-31 19:17:08 +0800

    feat: publish Codex Bank Teller PoC

---
+++ packages/browser-harness/src/index.ts
@@ -0,0 +1,9 @@
+export {
+  assertAllowedAction,
+  assertAllowedOrigin,
+  createBrowserPolicy,
+  detectPromptInjection,
+  type BrowserAction,
+  type BrowserPolicy,
+} from "./allowlist.js";
+export { InMemoryBrowserHarness } from "./harness.js";
